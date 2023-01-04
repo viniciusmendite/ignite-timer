f62c392ddadc3afab5a53e49387fc8ac57b3b903
Vinicius Mendite <vinicius@sharbe.com.br>
2023-01-04 20:28:39 -0300

    refactor: context between routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { Router } from './Router';
+import { CyclesContextProvider } from './contexts/CyclesContext';
 
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
@@ -10,7 +11,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
